bd75ccd25dcbefbdaa2e121a32e285e7fb802b29
plarke0 <plarke@byu.edu>
2026-01-19 01:21:03 -0700

    Implement king movement

---
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -0,0 +1,17 @@
+package chess;
+
+import java.util.Collection;
+
+public class KingMoveCalculator extends MoveCalculator{
+
+    public KingMoveCalculator(ChessPiece piece, ChessBoard board, ChessPosition piecePosition) {
+        super(piece, board, piecePosition);
+        this.movementOffsets = new int[][] {{-1,-1}, {0,-1}, {1,-1}, {-1,0},
+                                            {1,0}, {-1,1}, {0,1}, {1, 1}};
+        this.range = 1;
+    }
+
+    public Collection<ChessMove> calculateMoves() {
+        return validMovesAlongOffsets();
+    }
+}
