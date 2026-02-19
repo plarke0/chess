80f36c356f29fdcc72b15b62c8b594786cfcaab5
plarke0 <plarke@byu.edu>
2026-02-19 11:33:33 -0700

    Update interface method signatures

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -5,6 +5,6 @@ import model.AuthData;
 public interface AuthDAO {
     void insertAuth(AuthData authData) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
-    void deleteAuth(String authToken) throws DataAccessException;
+    void deleteAuth(AuthData authData) throws DataAccessException;
     void clear() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -8,6 +8,6 @@ public interface GameDAO {
     void insertGame(GameData gameData) throws DataAccessException;
     GameData getGame(String gameID) throws DataAccessException;
     Collection<GameData> listGames() throws DataAccessException;
-    void updateGame(String gameID, GameData gameData) throws DataAccessException;
+    void updateGame(GameData gameData) throws DataAccessException;
     void clear() throws DataAccessException;
 }
