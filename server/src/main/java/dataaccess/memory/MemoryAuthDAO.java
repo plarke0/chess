54ffe8c67dcbb6bf6973bf909799058d47814ef7
plarke0 <plarke@byu.edu>
2026-02-23 21:51:09 -0700

    Outline methods and initializer

--- server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
@@ -1,4 +1,31 @@
 package dataaccess.memory;
 
-public class MemoryAuthDAO {
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.memory.database.AuthDB;
+import model.AuthData;
+
+public class MemoryAuthDAO implements AuthDAO {
+
+    private final AuthDB authDB;
+
+    public MemoryAuthDAO(AuthDB authDB) {
+        this.authDB = authDB;
+    }
+
+    public void insertAuth(AuthData authData) throws DataAccessException {
+
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    public void deleteAuth(AuthData authData) throws DataAccessException {
+
+    }
+
+    public void clear() throws DataAccessException {
+
+    }
 }
