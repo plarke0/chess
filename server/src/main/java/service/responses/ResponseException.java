0552be86e007b35ce6dfa6bf8db8e7597ee93f78
plarke0 <plarke@byu.edu>
2026-02-24 00:13:41 -0700

    Add http staus attribute

--- server/src/main/java/service/responses/ResponseException.java
+++ server/src/main/java/service/responses/ResponseException.java
@@ -1,10 +1,17 @@
 package service.responses;
 
-public class ResponseException extends Exception{
+public class ResponseException extends Exception {
+
+    public int httpCode = 500;
+
     public ResponseException(String message) {
         super(message);
     }
     public ResponseException(String message, Throwable ex) {
         super(message, ex);
+    }
+    public ResponseException(int httpCode, String message) {
+        super(message);
+        this.httpCode = httpCode;
     }
 }
