d3c11f18dd5f6d8ba5776e5b3892a1f6bc8f3a15
plarke0 <plarke@byu.edu>
2026-02-23 18:00:29 -0700

    Implement data access methods

--- server/src/main/java/dataaccess/memory/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryUserDAO.java
@@ -1,4 +1,34 @@
 package dataaccess.memory;
 
-public class MemoryUserDAO {
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MemoryUserDAO implements UserDAO {
+
+    private ArrayList<UserData> userDB;
+
+    public MemoryUserDAO() {
+        this.userDB = new ArrayList<>();
+    }
+
+    public void insertUser(UserData userData) throws DataAccessException {
+        this.userDB.add(userData);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        for (UserData user : this.userDB) {
+            if (Objects.equals(username, user.username())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void clear() throws DataAccessException {
+        this.userDB = new ArrayList<>();
+    }
 }
