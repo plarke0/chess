bffd55f66d74bca1f52581693ef9cec89a265d0f
plarke0 <plarke@byu.edu>
2026-02-23 22:02:56 -0700

    Outline methods and initializer

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,6 +1,21 @@
 package service;
 
+import dataaccess.memory.database.AuthDB;
+import dataaccess.memory.database.GameDB;
+import dataaccess.memory.database.UserDB;
+
 public class ClearService {
+
+    private final AuthDB authDB;
+    private final UserDB userDB;
+    private final GameDB gameDB;
+
+    public ClearService(AuthDB authDB, UserDB userDB, GameDB gameDB) {
+        this.authDB = authDB;
+        this.userDB = userDB;
+        this.gameDB = gameDB;
+    }
+
     void clear() {
         throw new UnsupportedOperationException("Feature not implemented.");
     }
