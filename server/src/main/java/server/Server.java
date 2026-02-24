e182d9873a980afa40bf06dc2d79d86ecc6f47c8
plarke0 <plarke@byu.edu>
2026-02-23 17:12:25 -0700

    Add initialization for service classes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,27 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
+import service.requests.RegisterRequest;
+import service.responses.RegisterResponse;
 
 public class Server {
 
+    private final UserService userService;
+    private final GameService gameService;
+    private final ClearService clearService;
     private final Javalin javalin;
 
     public Server() {
+        this.userService = new UserService();
+        this.gameService = new GameService();
+        this.clearService = new ClearService();
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-        // Register your endpoints and exception handlers here.
+        javalin.post("/user", this::registerUser);
 
     }
 
@@ -20,5 +32,11 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void registerUser(Context context) throws Exception {
+        RegisterRequest registerRequest = new Gson().fromJson(context.body(), RegisterRequest.class);
+        RegisterResponse registerResponse = userService.register(registerRequest);
+        context.result(new Gson().toJson(registerResponse));
     }
 }
