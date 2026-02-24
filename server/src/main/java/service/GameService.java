ceed9570815611e40e94ea7f7f929550ac0d36df
plarke0 <plarke@byu.edu>
2026-02-23 22:07:40 -0700

    Implement initializer

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,5 +1,9 @@
 package service;
 
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryGameDAO;
+import dataaccess.memory.database.AuthDB;
+import dataaccess.memory.database.GameDB;
 import service.requests.CreateGameRequest;
 import service.requests.JoinGameRequest;
 import service.requests.ListGamesRequest;
@@ -7,6 +11,15 @@ import service.responses.CreateGameResponse;
 import service.responses.ListGamesResponse;
 
 public class GameService {
+
+    private final MemoryGameDAO gameDAO;
+    private final MemoryAuthDAO authDAO;
+
+    public GameService(GameDB gameDB, AuthDB authDB) {
+        this.gameDAO = new MemoryGameDAO(gameDB);
+        this.authDAO = new MemoryAuthDAO(authDB);
+    }
+
     CreateGameResponse createGame(CreateGameRequest createGameRequest) {
         throw new UnsupportedOperationException("Feature not implemented.");
     }
