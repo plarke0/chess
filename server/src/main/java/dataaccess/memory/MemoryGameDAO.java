b6361c05fd0fa7158b52cfb3d1f9935d8ae28d98
plarke0 <plarke@byu.edu>
2026-02-23 22:00:16 -0700

    Outline methods and initializer

--- server/src/main/java/dataaccess/memory/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryGameDAO.java
@@ -1,4 +1,38 @@
 package dataaccess.memory;
 
-public class MemoryGameDAO {
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.memory.database.GameDB;
+import model.GameData;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MemoryGameDAO implements GameDAO {
+
+    public GameDB gameDB;
+
+    public MemoryGameDAO(GameDB gameDB) {
+        this.gameDB = gameDB;
+    }
+
+    public void insertGame(GameData gameData) throws DataAccessException {
+
+    }
+
+    public GameData getGame(String gameID) throws DataAccessException {
+        return null;
+    }
+
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    public void updateGame(GameData gameData) throws DataAccessException {
+
+    }
+
+    public void clear() throws DataAccessException {
+
+    }
 }
