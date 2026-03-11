163a8b7a7156d3343200621b51ffda357b6b3979
plarke0 <plarke@byu.edu>
2026-03-10 23:51:44 -0600

    Implement getAuth

--- server/src/main/java/dataaccess/mysql/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/mysql/MySQLAuthDAO.java
@@ -13,7 +13,17 @@ public class MySQLAuthDAO implements AuthDAO {
     }
 
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(
+                    "SELECT users.username, auths.auth_token FROM auths JOIN users ON auths.user=users.id")) {
+                var rs = preparedStatement.executeQuery();
+                rs.next();
+                String username = rs.getString(1);
+                return new AuthData(authToken, username);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     public void deleteAuth(AuthData authData) throws DataAccessException {
