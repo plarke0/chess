837581f4ef1027811ceed21cefcdcbe53b85a9cb
plarke0 <plarke@byu.edu>
2026-03-11 01:13:03 -0600

    Implement insert game

--- server/src/main/java/dataaccess/mysql/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/mysql/MySQLGameDAO.java
@@ -1,18 +1,37 @@
 package dataaccess.mysql;
 
+import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import dataaccess.GameDAO;
 import model.GameData;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.List;
 
 public class MySQLGameDAO implements GameDAO {
     public int insertGame(GameData gameData) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(
+                    "INSERT INTO games (game_name, white_user, black_user, board) VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, gameData.gameName());
+                preparedStatement.setString(2, gameData.whiteUsername());
+                preparedStatement.setString(3, gameData.blackUsername());
+                preparedStatement.setString(4, new Gson().toJson(gameData.game()));
+                preparedStatement.executeUpdate();
 
-        return 0;
+                var resultSet = preparedStatement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    return resultSet.getInt(1);
+                } else {
+                    throw new DataAccessException("New game id unable to be generated");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     public GameData getGame(int gameID) throws DataAccessException {
