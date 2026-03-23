eebdc3acbc53790230f6b286ebf8bd5b7b036a01
plarke0 <plarke@byu.edu>
2026-03-22 22:00:53 -0600

    Outline methods

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -5,4 +5,37 @@ import java.nio.charset.StandardCharsets;
 import ui.EscapeSequences.*;
 
 public class ChessBoard {
+
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+    private static final int SQUARE_SIZE_IN_CHARACTERS = 1;
+
+    private static PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+    public static void drawBoard() {
+
+    }
+
+    private static void drawColumnHeaders() {
+
+    }
+
+    private static void drawRowHeaders() {
+
+    }
+
+    private static void drawBoardRow() {
+
+    }
+
+    private static void drawBoardSquare() {
+
+    }
+
+    private static void setSquareColor() {
+
+    }
+
+    private static void drawChessPiece() {
+
+    }
 }
