1b18ad1b7deafe4ed34a56ef0cee87f777350f97
plarke0 <plarke@byu.edu>
2026-03-24 17:12:50 -0600

    Add method to handle json bodies

--- shared/src/main/java/responses/ResponseException.java
+++ shared/src/main/java/responses/ResponseException.java
@@ -1,5 +1,9 @@
 package responses;
 
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
 public class ResponseException extends Exception {
 
     public int httpCode = 500;
@@ -13,5 +17,11 @@ public class ResponseException extends Exception {
     public ResponseException(int httpCode, String message) {
         super(message);
         this.httpCode = httpCode;
+    }
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = ((Double)map.get("status")).intValue();
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
     }
 }
