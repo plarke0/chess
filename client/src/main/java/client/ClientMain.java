c4ed3061cf311571c15a416b1eee8187e899dc39
plarke0 <plarke@byu.edu>
2026-03-24 12:02:54 -0600

    Start repl

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,15 @@
 package client;
 
 import chess.*;
+import ui.repl.REPL;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverURL = "http://localhost:8080";
+        if (args.length == 1) {
+            serverURL = args[0];
+        }
+        REPL repl = new REPL(serverURL);
+        repl.run();
     }
 }
