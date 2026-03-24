ba30342d2ba39827fcf143f3435ed247f4f7a8a5
plarke0 <plarke@byu.edu>
2026-03-24 17:32:35 -0600

    Implement communicator methods

--- client/src/main/java/client/ClientCommunicator.java
+++ client/src/main/java/client/ClientCommunicator.java
@@ -1,27 +1,67 @@
 package client;
 
+import com.google.gson.Gson;
 import responses.ResponseException;
 
+import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ClientCommunicator {
+    private final HttpClient client = HttpClient.newHttpClient();
     private String serverURL;
 
+    private <T> T makeRequest(String method, String path, Object body, Class<T> responseClass) throws ResponseException {
+        try {
+            var request = buildRequest(method, path, body);
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return handleResponse(response, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
     public HttpRequest buildRequest(String method, String path, Object body) {
-        return null;
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverURL + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
     }
 
     private HttpRequest.BodyPublisher makeRequestBody(Object request) {
-        return null;
-    }
-
-    public HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
-        return null;
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
     }
 
     public <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
-        return null;
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+
+        if (responseClass == null) {
+            return null;
+        }
+
+        return new Gson().fromJson(response.body(), responseClass);
+    }
+
+    private Boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 
     public void setServerURL(String serverURL) {
