39e0dd97d65d2fc18fb0497fe29c9571b47033fb
plarke0 <plarke@byu.edu>
2026-03-25 00:56:55 -0600

    Implement help

--- client/src/main/java/ui/repl/clients/GameClient.java
+++ client/src/main/java/ui/repl/clients/GameClient.java
@@ -4,6 +4,8 @@ import client.ServerFacade;
 import model.GameData;
 import ui.ChessBoard;
 
+import java.util.Arrays;
+
 public class GameClient implements Client{
 
     private final String serverURL;
@@ -15,7 +17,18 @@ public class GameClient implements Client{
     }
 
     public ClientResponse eval(String line, ClientData currentClientData) {
-        return null;
+        try {
+            String[] args = line.toLowerCase().split(" ");
+            String cmd = (args.length > 0) ? args[0] : null;
+            String[] params = Arrays.copyOfRange(args, 1, args.length);
+            return switch (cmd) {
+                case "help" -> help();
+                case null, default -> unrecognisedCommand(cmd);
+            };
+        } catch (IllegalArgumentException ex) {
+            String msg = ex.getMessage();
+            return new ClientResponse(null, null, msg);
+        }
     }
 
     public String getPromptTitle() {
@@ -23,12 +36,32 @@ public class GameClient implements Client{
     }
 
     private ClientResponse help() {
-        return null;
+        String msg = """
+                More to come in Phase 6
+                exit - leave the game
+                help - list possible commands
+                """;
+        return new ClientResponse(null, null, msg);
     }
 
     private ClientResponse exit() {
         return null;
     }
+
+    private ClientResponse unrecognisedCommand(String command) throws IllegalArgumentException {
+        if (command == null || command.isEmpty()) {
+            throw new IllegalArgumentException("No command was provided. Type 'help' for a list of available commands.");
+        } else {
+            throw new IllegalArgumentException("'" + command + "' is not a valid command. Type 'help' for a list of available commands.");
+        }
+    }
+
+    private void validateCommand(String[] params, int argCount) throws IllegalArgumentException {
+        if (params.length < argCount) {
+            throw new IllegalArgumentException("Only " + params.length + " arguments were given when " + argCount + " was/were needed.");
+        }
+    }
+
     public void drawBoard(ClientData clientData) {
         String user = clientData.getUsername();
         GameData activeGame = clientData.getActiveGame();
