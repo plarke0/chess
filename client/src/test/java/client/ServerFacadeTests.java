45ea8e3492f4f6169ca19e2a58717d66e87cdc86
plarke0 <plarke@byu.edu>
2026-03-25 01:49:17 -0600

    Outline tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -7,11 +7,13 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
+        serverFacade = new ServerFacade("http://localhost:" + port);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -22,8 +24,67 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
+    public void clearPositive() {
         Assertions.assertTrue(true);
     }
 
+    @Test
+    public void registerUserPositive() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void registerUserNegative() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void loginUserPositive() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void loginUserNegative() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void logoutUserPositive() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void logoutUserNegative() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void listGamesPositive() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void listGamesNegative() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void createGamePositive() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void createGameNegative() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void joinGamePositive() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void joinGameNegative() {
+        Assertions.assertTrue(true);
+    }
 }
