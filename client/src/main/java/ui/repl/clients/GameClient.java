a90d9593ee6f7f79c130771d14d63dea0b96645b
plarke0 <plarke@byu.edu>
2026-04-09 03:43:53 -0600

    Outline commands and reorganize methods

--- client/src/main/java/ui/repl/clients/GameClient.java
+++ client/src/main/java/ui/repl/clients/GameClient.java
@@ -27,8 +27,12 @@ public class GameClient implements Client{
             String cmd = (args.length > 0) ? args[0] : null;
             String[] params = Arrays.copyOfRange(args, 1, args.length);
             return switch (cmd) {
+                case "redraw" -> redraw();
+                case "highlight" -> highlight();
+                case "move" -> move();
+                case "leave" -> leave();
+                case "resign" -> resign();
                 case "help" -> help();
-                case "exit" -> exit();
                 case null, default -> unrecognisedCommand(cmd);
             };
         } catch (IllegalArgumentException ex) {
@@ -41,18 +45,37 @@ public class GameClient implements Client{
         return "IN_GAME";
     }
 
-    private ClientResponse help() {
-        String msg = """
-                exit - leave the game
-                help - list possible commands
-                More to come in Phase 6""";
-        return new ClientResponse(null, null, msg);
+    private ClientResponse redraw() {
+        return null;
     }
 
-    private ClientResponse exit() {
+    private ClientResponse highlight() {
+        return null;
+    }
+
+    private ClientResponse move() {
+        return null;
+    }
+
+    private ClientResponse leave() {
         ClientData newClientData = new ClientData(currentClientData.getUsername(), currentClientData.getAuthToken(), null);
         // TODO: Add check for if gameName is null
         return new ClientResponse("signedInClient", newClientData, "Left '" + currentClientData.getActiveGame().gameName() + "'");
+    }
+
+    private ClientResponse resign() {
+        return null;
+    }
+
+    private ClientResponse help() {
+        String msg = """
+                redraw - redraws the chess board
+                highlight <source> - highlights the available moves of the piece at source
+                move <source> <destination> <promotion (optional)> - moves the piece at source to destination
+                leave - leave the game
+                resign - resign the game
+                help - list possible commands""";
+        return new ClientResponse(null, null, msg);
     }
 
     public void drawBoard(ClientData clientData) {
