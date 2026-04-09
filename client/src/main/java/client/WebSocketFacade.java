13195e7128b70c538e7a18d321ce57fa50be7309
plarke0 <plarke@byu.edu>
2026-04-09 01:39:03 -0600

    Outline facade functionality

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -1,4 +1,52 @@
 package client;
 
-public class WebSocketFacade {
+import jakarta.websocket.ContainerProvider;
+import jakarta.websocket.Endpoint;
+import jakarta.websocket.EndpointConfig;
+import jakarta.websocket.MessageHandler;
+import jakarta.websocket.Session;
+import jakarta.websocket.WebSocketContainer;
+import ui.repl.REPL;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class WebSocketFacade extends Endpoint{
+    public Session session;
+    private final REPL repl;
+
+    public WebSocketFacade(REPL repl) throws Exception {
+        URI uri = new URI("ws://localhost:8080/ws");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        session = container.connectToServer(this, uri);
+
+        this.repl = repl;
+        this.session.addMessageHandler(new MessageHandler.Whole<ServerMessage>() {
+            public void onMessage(ServerMessage message) {
+                // Check message type and call the corresponding handler method
+            }
+        });
+    }
+
+    private void handleNotification(ServerMessage notificationMessage) {
+
+    }
+
+    private void handleError(ServerMessage errorMessage) {
+
+    }
+
+    private void handleLoadGame(ServerMessage loadGameMessage) {
+
+    }
+
+    public void sendCommand(UserGameCommand command) throws IOException {
+        session.getBasicRemote().sendText(command.toString());
+    }
+
+    public void onOpen(Session session, EndpointConfig endpointConfig) {}
 }
